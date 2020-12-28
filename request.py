b9e8c7e9e8689b8c62c4807d5bd40e40f3683891
rezakarim1585 <74957734+rezakarim1585@users.noreply.github.com>
2020-12-28 16:15:40 +0600

    Update request.py

--- request.py
+++ request.py
@@ -4,6 +4,6 @@ import requests
 #['a/d', 'pf', 'fc', 'd', 'bw', 'Ef', 'Vexp']
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'a_d':1, 'pf':1, 'fc':1, 'd' : 0, 'b' : 1, 'ef' : 1})
+r = requests.post(url,json={'a_d':3, 'pf':4, 'fc':2, 'd' : 1, 'b' : 0, 'ef' : 5})
 
 print(r.json())
